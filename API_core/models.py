1b6f7adf32257b7495a59eb84df9dcb9ff14a1f1
William Cáceres <willycaceres2002@gmail.com>
2023-05-13 17:37:46 -0600

    New models incorporate API_CRUD_complete

--- API_core/models.py
+++ API_core/models.py
@@ -1,12 +1,50 @@
 from django.db import models
 
+
 # Create your models here.
+class Equipment_Category(models.Model):
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=200)
+
+class Equipment(models.Model):
+    name=models.CharField(max_length=30)
+    id_equipment_category=models.ForeignKey(Equipment_Category,on_delete=models.CASCADE)#establecemos la relacion
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=200)
 
 class Products(models.Model):
     #the Id is for default
     name=models.CharField(max_length=30)
-    description=models.CharField(max_length=30)
+    description=models.CharField(max_length=200)
     stock=models.IntegerField()
     cost=models.FloatField()
     price=models.FloatField()
-    #Products category pendiente
+    id_category=models.ForeignKey(Category,on_delete=models.CASCADE)#Cada producto pertence a una categoria
+
+class Bill_state(models.Model):
+    name=models.CharField(max_length=10)
+
+class Month(models.Model):
+    name=models.CharField(max_length=30)
+
+class Year(models.Model):
+    name=models.CharField(max_length=30)
+
+class Bill(models.Model):
+    date=models.DateField()
+    customer_name=models.CharField(max_length=50)
+    sub_total=models.FloatField()
+    iva=models.FloatField()
+    total=models.FloatField()
+    id_month=models.ForeignKey(Month, on_delete=models.CASCADE)
+    id_year=models.ForeignKey(Year, on_delete=models.CASCADE)
+    id_bill_state=models.ForeignKey(Bill_state,on_delete=models.CASCADE)
+
+class Sale(models.Model):
+    id_bill=models.ForeignKey(Bill, on_delete=models.CASCADE)
+    id_products=models.ForeignKey(Products,on_delete=models.CASCADE)
+    amount_products=models.IntegerField()
+    price_at_moment=models.FloatField()
+    total_sale=models.FloatField()
--- API_core/serializers.py
+++ API_core/serializers.py
@@ -1,9 +1,66 @@
 from rest_framework import serializers
-from .models import Products
+from .models import Products, Equipment, Equipment_Category, Category, Bill, Bill_state, Sale, Month, Year
+
+class EquipmentCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Equipment_Category
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name','description')
+        read_only_fields=('id',)
+
+class EquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Equipment
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name','id_equipment_category')
+        read_only_fields=('id',)
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Category
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name','description')
+        read_only_fields=('id',)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model=Products
         #campos que van a ser consultados que ya tiene las tablas
-        fields=('id','name','description','stock','cost','price')
+        fields=('id','name','description','stock','cost','price','id_category')
+        read_only_fields=('id',)
+
+class BillStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Bill_state
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name')
+        read_only_fields=('id',)
+
+class MonthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Month
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name')
+        read_only_fields=('id',)
+
+class YearSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Year
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','name')
+        read_only_fields=('id',)
+
+class BillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Bill
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','date','customer_name','sub_total','iva','total', 'id_month', 'id_year', 'id_bill_state')
+        read_only_fields=('id',)
+
+class SaleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Sale
+        #campos que van a ser consultados que ya tiene las tablas
+        fields=('id','id_bill','id_products','amount_products','price_at_moment','total_sale')
         read_only_fields=('id',)
