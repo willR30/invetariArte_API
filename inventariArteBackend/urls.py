fdd6267ecaabf90c99c323d99589a825e12d0efa
Gabriela <magabriela0715@gmai.com>
2023-05-24 11:57:10 -0600

    nuevo modelo accounts que contiene un profile view y la creacion de un token al usuario autenticado que es el administrador.

--- inventariArteBackend/urls.py
+++ inventariArteBackend/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('', include('API_core.urls'))
+    path('', include('API_core.urls')),
+    path('api/auth/', include('rest_framework.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
